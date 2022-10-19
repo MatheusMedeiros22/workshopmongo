e462d695cd0c6d6f787982609bd705660924ea35
Matheus Medeiros <mathmed22@gmail.com>
2022-10-18 23:27:35 -0300

    Obtendo um usuário por id

--- src/main/java/com/devsuperior/workshopmongo/resources/UserResources.java
+++ src/main/java/com/devsuperior/workshopmongo/resources/UserResources.java
@@ -1,25 +1,28 @@
 package com.devsuperior.workshopmongo.resources;
 
-import com.devsuperior.workshopmongo.domain.User;
 import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResources {
     @Autowired
     private UserService userService;
-
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll(){
         return ResponseEntity.ok(userService.findAll());
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        return ResponseEntity.ok(userService.findById(id));
     }
 }
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,19 +3,29 @@ package com.devsuperior.workshopmongo.services;
 import com.devsuperior.workshopmongo.domain.User;
 import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repository.UserRepository;
+import com.devsuperior.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
 
     public List<UserDTO> findAll(){
         List<User> userList = userRepository.findAll();
         return userList.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    }
+    public UserDTO findById(String id){
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            return new UserDTO(user.get());
+        }
+        throw new ObjectNotFoundException("Objeto não foi encontrado");
     }
 }
