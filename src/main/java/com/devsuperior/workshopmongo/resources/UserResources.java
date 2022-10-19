528e4d0040a58226018c82a0c5897170001827ce
Matheus Medeiros <mathmed22@gmail.com>
2022-10-18 22:32:26 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/devsuperior/workshopmongo/resources/UserResources.java
+++ src/main/java/com/devsuperior/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.devsuperior.workshopmongo.resources;
 
 import com.devsuperior.workshopmongo.domain.User;
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -17,7 +19,7 @@ public class UserResources {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-       return ResponseEntity.ok(userService.findAll());
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
     }
 }
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -1,18 +1,21 @@
 package com.devsuperior.workshopmongo.services;
 
 import com.devsuperior.workshopmongo.domain.User;
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
-        return userRepository.findAll();
+    public List<UserDTO> findAll(){
+        List<User> userList = userRepository.findAll();
+        return userList.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
     }
 }
